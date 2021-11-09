a0d1c6bad568a94dda648f6b542a44e50053474e
Taylor Brent <taylor.w.brent@gmail.com>
2021-11-09 16:03:25 -0400

    add ability to have different uri/royalty/artist per NFT

--- contracts/CancerNFT.sol
+++ contracts/CancerNFT.sol
@@ -42,51 +42,86 @@ contract CancerNFT is ERC721, IERC2981, Ownable {
     bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
     mapping(bytes4 => bool) private _supportedInterfaces;
 
-
-    // 100 max
-    uint8 public royaltyFraction = 10; // 10% initially
-
     address public royaltyDestination;
+    uint8 public defaultRoyaltyFraction = 10; // 10 / 100 = 10%
 
-    string public uri;
+    //
 
+    struct Info {
+        address artist;
+        uint8 royaltyFraction;
+        string uri;
+    }
+    mapping(uint256 => Info) nfts;    
     uint256 public collectionSize;
 
-    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+    string public licenseURI;
+
+    constructor(string memory name_, string memory symbol_, string memory licenseURI_) ERC721(name_, symbol_) {
         _registerInterface(_INTERFACE_ID_ERC2981);
         _registerInterface(_INTERFACE_ID_ERC721);
         _registerInterface(_INTERFACE_ID_ERC721_METADATA);
         transferOwnership(_msgSender());
         royaltyDestination = address(new Splitter(_msgSender()));
+        licenseURI = licenseURI_;
     }
 
-    function setRoyaltyDestination(address royaltyDestination_) external onlyOwner {
+    function setArtist(uint256 tokenId, address artist) external onlyOwner {
+        nfts[tokenId].artist = artist;
+    }
+
+    function setRoyaltyFraction(uint256 tokenId, uint8 royaltyFraction) external onlyOwner {
+        nfts[tokenId].royaltyFraction = royaltyFraction;
+    }
+
+    function setURI(uint256 tokenId, string memory uri_) external onlyOwner {
+        nfts[tokenId].uri = uri_;
+    }
+
+    //
+
+    function setDefaultRoyaltyFraction(uint8 royaltyFraction) external onlyOwner {
+        defaultRoyaltyFraction = royaltyFraction;
+    }
+
+    function setDefaultRoyaltyDestination(address royaltyDestination_) external onlyOwner {
         royaltyDestination = royaltyDestination_;
     }
 
-    function setRoyaltyFraction(uint8 royaltyFraction_) external onlyOwner {
-        require(royaltyFraction_ <= 50, "50% max royalty");
-        royaltyFraction = royaltyFraction_;
-    }
-
-    function mint(address to, uint256 amount) external onlyOwner {
+    /// Mints a batch of NFTs but requires the user to come through later and set NFT-specific data
+    function mintBatch(address to, uint256 amount) external onlyOwner {
         for (uint256 i = 0; i < amount; i++) {
-            _safeMint(to, collectionSize);
-            collectionSize++;
+            _mintHelper(to, address(0), defaultRoyaltyFraction, "");
         }
     }
 
-    function royaltyInfo(uint256, uint256 _salePrice) external view returns (address receiver, uint256 royaltyAmount) {
-        return (royaltyDestination, uint256(_salePrice * royaltyFraction / 100));
+    /// Mints a single NFT and sets NFT-specific data at the same time
+    function mint(address to, address artist, uint8 royaltyFraction, string calldata uri) external onlyOwner {
+        _mintHelper(to, artist, royaltyFraction, uri);
     }
 
-    function setURI(string memory uri_) external onlyOwner {
-        uri = uri_;
+    function royaltyInfo(uint256 tokenId, uint256 _salePrice) external view returns (address receiver, uint256 royaltyAmount) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        // TODO: Artist slice
+        return (royaltyDestination, uint256(_salePrice * nfts[tokenId].royaltyFraction / 100));
     }
 
 
-    function _baseURI() internal view override returns (string memory) {
-        return uri;
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+        return nfts[tokenId].uri;
+    }
+
+    function checkNFTInitialized(uint256 tokenId) external view returns (bool) {
+        return nfts[tokenId].artist != address(0) && keccak256(abi.encodePacked(nfts[tokenId].uri)) != keccak256(abi.encodePacked(""));
+    }
+
+    //
+
+    function _mintHelper(address to, address artist, uint8 royaltyFraction, string memory uri) internal {
+        _safeMint(to, collectionSize);
+        nfts[collectionSize] = Info(artist, royaltyFraction, uri);
+        collectionSize++;
     }
 
     function _registerInterface(bytes4 interfaceId) internal {
